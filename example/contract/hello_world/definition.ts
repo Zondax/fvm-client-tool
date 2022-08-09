49d31003590e9ed27c228a86cb337b6b2947ad2d
Emmanuel <emmanuelm41@gmail.com>
2022-08-09 16:29:14 -0300

    feature: add support for custom types on arguments

--- example/contract/hello_world/definition.ts
+++ example/contract/hello_world/definition.ts
@@ -2,5 +2,8 @@ type Account = { address: string; private_base64: string };
 
 export interface Contract{
 	new : (account: Account, value: string, ) => Promise<string>
-	say_hello : (account: Account, value: string, data1:BigInt[], data2:string[], data3:{[key:string]: BigInt}) => Promise<string>
+	say_hello : (account: Account, value: string, data1:BigInt[], data2:string[], data3:{[key:string]: BigInt}, data4:CustomArgument) => Promise<string>
 }
+export type CustomArgument = { field1: BigInt
+field2: number
+field3: string }
